90a898d46ef47d65db160e9816aadd154be9ebb3
jungmin.park <pjm9673@gmail.com>
2026-03-11 14:55:38 +0900

    오류 task이름변경

--- dags/dags_python_show_templates.py
+++ dags/dags_python_show_templates.py
@@ -4,13 +4,13 @@ import datetime
 from airflow.decorators import task
 
 with DAG(
-    dag_id="dags_bash_with_template",
+    dag_id="dags_python_show_templates",
     schedule="10 0 * * *",
     start_date=pendulum.datetime(2023, 3, 10, tz="Asia/Seoul"),
     catchup=True
 ) as dag:
     @task(task_id='python_task')
-    def show_templates(**kwargs)
+    def show_templates(**kwargs):
         from pprint import pprint
         pprint(kwargs)
 
