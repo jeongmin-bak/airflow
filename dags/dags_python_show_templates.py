5b9ede8f95b07a7f44b0958da6319f51c74e7ab0
jungmin.park <pjm9673@gmail.com>
2026-03-11 14:54:54 +0900

    python task date test

---
+++ dags/dags_python_show_templates.py
@@ -0,0 +1,17 @@
+from airflow import DAG 
+import pendulum 
+import datetime 
+from airflow.decorators import task
+
+with DAG(
+    dag_id="dags_bash_with_template",
+    schedule="10 0 * * *",
+    start_date=pendulum.datetime(2023, 3, 10, tz="Asia/Seoul"),
+    catchup=True
+) as dag:
+    @task(task_id='python_task')
+    def show_templates(**kwargs)
+        from pprint import pprint
+        pprint(kwargs)
+
+    show_templates()
