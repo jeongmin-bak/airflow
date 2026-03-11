a30c81c79d74c7c5c246e68ffba0eddc4c3db561
jungmin.park <pjm9673@gmail.com>
2026-03-11 13:42:56 +0900

    dag 이름변경

--- dags/dags_python_with_op_arg.py
+++ dags/dags_python_with_op_arg.py
@@ -5,7 +5,7 @@ from airflow.providers.standard.operators.python import PythonOperator
 from common.common_func import regist
 
 with DAG(
-    dag_id="dags_python_import_func",
+    dag_id="dags_python_with_op_arg",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
