e28db9d0be32f9f815c9c65866272c44f7370161
jungmin.park <pjm9673@gmail.com>
2026-03-12 00:33:43 +0900

    문법 오류 수정

--- dags/dags_python_with_macro.py
+++ dags/dags_python_with_macro.py
@@ -9,9 +9,10 @@ with DAG(
     catchup=False
 ) as dag:
     @task(task_id='task_using_macros',
-            templates_dict={'start_date': '{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(months=-1, days=1) | ds}}',
-                            'end_date' : '{{ (data_interval_end.in_timezone("Asia/Seoul").replace(day=1) - macros.dateutil.relativedelta.relativedelta(days=-1) | ds}}'
-    })
+      templates_dict={'start_date':'{{ (data_interval_end.in_timezone("Asia/Seoul") + macros.dateutil.relativedelta.relativedelta(months=-1, day=1)) | ds }}',
+                      'end_date': '{{ (data_interval_end.in_timezone("Asia/Seoul").replace(day=1) + macros.dateutil.relativedelta.relativedelta(days=-1)) | ds }}'
+     }
+    )
     def get_datetime_macro(**kwargs):
         templates_dict = kwargs.get('templates_dict') or {}
         if templates_dict:
