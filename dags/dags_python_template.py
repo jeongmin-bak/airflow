02aa2f351e3b5e3b26ca0cea71ad955f34eee4b0
jungmin.park <pjm9673@gmail.com>
2026-03-11 15:14:00 +0900

    task 이름 변경

--- dags/dags_python_template.py
+++ dags/dags_python_template.py
@@ -5,7 +5,7 @@ from airflow.decorators import task
 from airflow.providers.standard.operators.python import PythonOperator
 
 with DAG(
-    dag_id="dags_python_show_templates",
+    dag_id="dags_python_templates",
     schedule="10 0 * * *",
     start_date=pendulum.datetime(2026, 3, 10, tz="Asia/Seoul"),
     catchup=True
