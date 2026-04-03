e1b5de8a53befd6c48e800777ffb665f01043c11
jungmin.park <pjm9673@gmail.com>
2026-04-04 00:33:54 +0900

    return value None 수정

--- dags/dags_bash_python_with_xcom_2.py
+++ dags/dags_bash_python_with_xcom_2.py
@@ -36,7 +36,7 @@ with DAG(
     def python_pull_xcom(**kwargs):
         ti = kwargs['ti']
         status_value = ti.xcom_pull(key='bash_pushed', task_ids='bash_push')
-        return_value = ti.xcom_pull(key='bash_push')
+        return_value = ti.xcom_pull(task_ids='bash_push')
         print('status_value: ' + str(status_value))
         print('return_value: ' + str(return_value))
 
