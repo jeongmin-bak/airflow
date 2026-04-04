3b669a0eaa4cabfe9f3123ffb061d55576e0b5af
jungmin.park <pjm9673@gmail.com>
2026-04-04 22:22:56 +0900

    str 오류 수정

--- dags/dags_branch_python_operator.py
+++ dags/dags_branch_python_operator.py
@@ -14,21 +14,20 @@ with DAG(
     def select_random():
         import random
 
-        item_lst = ['A', 'B', 'C']
+        item_lst = ['A','B','C']
         selected_item = random.choice(item_lst)
         if selected_item == 'A':
             return 'task_a'
-        elif selected_item in ['B', 'C']:
-            return ['task_b', 'task_c']
-        
+        elif selected_item in ['B','C']:
+            return ['task_b','task_c']
+    
     python_branch_task = BranchPythonOperator(
         task_id='python_branch_task',
         python_callable=select_random
     )
-        
+    
     def common_func(**kwargs):
         print(kwargs['selected'])
-    
 
     task_a = PythonOperator(
         task_id='task_a',
@@ -45,7 +44,7 @@ with DAG(
     task_c = PythonOperator(
         task_id='task_c',
         python_callable=common_func,
-        op_kwargs={'selected':'c'}
+        op_kwargs={'selected':'C'}
     )
 
-    python_branch_task >> ['task_a', 'task_b', 'task_c']
+    python_branch_task >> [task_a, task_b, task_c]
