28227593550ea0d4e0eb8c8ed99616d1576369a0
jungmin.park <pjm9673@gmail.com>
2026-04-04 22:23:47 +0900

    오류 수정

--- dags/dags_python_with_branch_decorator.py
+++ dags/dags_python_with_branch_decorator.py
@@ -3,32 +3,29 @@ import pendulum
 from airflow.providers.standard.operators.python import PythonOperator
 from airflow.sdk import DAG, task
 
-
 with DAG(
     dag_id='dags_python_with_branch_decorator',
+    start_date=datetime(2023,4,1),
     schedule=None,
-    start_date=pendulum.datetime(2023, 4, 1, tz='Asia/Seoul'),
     catchup=False
 ) as dag:
     @task.branch(task_id='python_branch_task')
     def select_random():
         import random
-
         item_lst = ['A', 'B', 'C']
         selected_item = random.choice(item_lst)
         if selected_item == 'A':
             return 'task_a'
-        elif selected_item in ['B', 'C']:
-            return ['task_b', 'task_c']
-        
+        elif selected_item in ['B','C']:
+            return ['task_b','task_c']
+    
     def common_func(**kwargs):
         print(kwargs['selected'])
-    
 
     task_a = PythonOperator(
         task_id='task_a',
         python_callable=common_func,
-        op_kwarg={'selected':'A'}
+        op_kwargs={'selected':'A'}
     )
 
     task_b = PythonOperator(
@@ -40,7 +37,7 @@ with DAG(
     task_c = PythonOperator(
         task_id='task_c',
         python_callable=common_func,
-        op_kwargs={'selected':'c'}
+        op_kwargs={'selected':'C'}
     )
 
-    select_random() >> ['task_a', 'task_b', 'task_c']
+    select_random() >> [task_a, task_b, task_c]
