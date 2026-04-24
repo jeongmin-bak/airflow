717aba3174e962ce6a781ad3e334c12a78e4bcd7
jungmin.park <pjm9673@gmail.com>
2026-04-24 14:57:46 +0900

    공공데이터 땡기기

---
+++ dags/dags_simple_http_operator.py
@@ -0,0 +1,37 @@
+import pendulum
+from airflow.providers.http.operators.http import HttpOperator
+from airflow.sdk import DAG, task 
+
+
+with DAG (
+    dag_id = 'dags_simple_http_operator',
+    start_date=pendulum.datetime(2023, 4, 1, tz='Asia/Seoul'),
+    catchup=False,
+    schedule=None,
+) as dag:
+
+    tb_cycle_station_info = HttpOperator (
+        task_id = 'tb_cycle_station_info',
+        http_conn_id='openapi.seoul.go.kr',
+        endpoint='{{var.value.apikey_openapi_seoul_go_kr}}/json/tbCycleStationInfo/1/10/',
+        method='GET',
+        headers={'Content-Type': 'application/json',
+                        'charset': 'utf-8',
+                        'Accept': '*/*'
+                        }
+    )
+
+    @task(task_id='python_2')
+    def python_2(**kwargs):
+        ti = kwargs['ti']
+        rslt = ti.xcom_pull(task_id='tb_cycle_station_info')
+        import json 
+        from pprint import pprint 
+
+        pprint(ti)
+        pprint(json.loads(rslt))
+
+    
+    tb_cycle_station_info >> python_2()
+
+    
