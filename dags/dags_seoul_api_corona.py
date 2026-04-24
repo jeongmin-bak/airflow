a86351e0feb4a695c55b1b54097c8af70df6c6e0
jungmin.park <pjm9673@gmail.com>
2026-04-24 16:37:19 +0900

    오타 수정

--- dags/dags_seoul_api_corona.py
+++ dags/dags_seoul_api_corona.py
@@ -10,10 +10,10 @@ with DAG(
     catchup=False
 ) as dag:
     '''서울시 코로나19 확진자 발생동향'''
-    tb_corona19_count_status = SeoulApiToCsvOperator (
+    tb_corona19_count_status = SeoulApiToCsvOperator(
         task_id = 'tb_corona19_count_status',
         dataset_nm='TbCorona19CountStatus',
-        path='/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}'
+        path='/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
         file_name='TbCorona19CountStatus.csv'
     )
 
