b3d19f5c2453a0018322de366a5d2bc3d554809b
jungmin.park <pjm9673@gmail.com>
2026-05-04 17:12:56 +0900

    dataset test2

--- dags/dags_dataset_consumer_2.py
+++ dags/dags_dataset_consumer_2.py
@@ -4,15 +4,16 @@ from airflow.sdk import DAG
 from airflow.datasets import Dataset
 
 dataset_dags_dataset_producer_1 = Dataset("dags_dataset_producer_1")
+dataset_dags_dataset_producer_2 = Dataset("dags_dataset_producer_2")
 
 with DAG(
-        dag_id='dags_dataset_consumer_1',
-        schedule=[dataset_dags_dataset_producer_1],
+        dag_id='dags_dataset_consumer_2',
+        schedule=[dataset_dags_dataset_producer_1, dataset_dags_dataset_producer_2],
         start_date=pendulum.datetime(2023, 4, 1, tz='Asia/Seoul'),
         catchup=False,
         tags=['asset','producer']
 ) as dag:
     bash_task = BashOperator(
         task_id = 'bash_task',
-        bash_command='echo {{ ti.run_id }} && echo "producer_1 이 완료되면 수행"'
+        bash_command='echo {{ ti.run_id }} && echo "producer_1 과 producer_2 완료되면 수행"'
     )
